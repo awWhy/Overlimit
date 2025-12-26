6ca3bb274f45dfafa042e33315cbd828cb15a340
awWhy <107661324+awWhy@users.noreply.github.com>
2025-12-26 20:15:26 +0000

    Deploying to gh-pages from @ awWhy/Overlimit@ab3bece8c6df3f9e49a0824c47e47262ad349ce0 🚀

--- Overlimit.js
+++ Overlimit.js
@@ -747,10 +747,7 @@ const technical = {
       digits = powerCheck < 1 ? baseSettings.maxDigits : Math.max(baseSettings.maxDigits - powerCheck, baseSettings.minDigits);
     }
     let formated = padding ? mantissa.toFixed(digits) : `${mantissa}`;
-    if (type === "input") {
-      return formated;
-    }
-    if (mantissa >= 1e3) {
+    if (type !== "input" && mantissa >= 1e3) {
       let index = formated.indexOf(".");
       if (index !== -1) {
         formated = `${formated.slice(0, index)}${defaultSettings.point}${formated.slice(index + 1)}`;
